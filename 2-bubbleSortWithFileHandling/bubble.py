01380d145415d37a8b180e6c1951e1d5fedb5f3f
Savina Weerasooriya <savi.sihasara@gmail.com>
2020-05-02 22:30:40 +0530

    Bubble sorting a text

---
+++ 2-bubbleSortWithFileHandling/bubble.py
@@ -0,0 +1,23 @@
+file = open('num.txt' , "r")
+x = file.readline()
+
+while file:
+	x = x.strip().split(",")#making a list
+	l = len(x)
+
+	#converting to integer
+	for m in range(l):
+		x[m] = int(x[m])
+
+	print("The unsorted list is ", x)
+
+	#bubble sorting
+	for y in range(l):
+		for z in range(l-y-1):
+			if x[z]>x[z+1]:
+				x[z],x[z+1] = x[z+1],x[z]
+
+	print("List after sorted >> ",x)
+	print('\n')
+
+	x = file.readline()
